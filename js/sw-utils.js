c532a28b8fdf7ede3c9145917ca798adb4b8d202
cgarcia-apt <carlos.garcia@atcpj.com>
2020-05-07 12:03:47 -0500

    first

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+function ActualizaCacheDinamico( dinamicCache, req, res){
+   
+    if(res.ok){
+        return caches.open(dinamicCache).then( cache => {
+            cache.put( req, res.clone() );
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+
+}
