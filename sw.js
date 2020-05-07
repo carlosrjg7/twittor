d12b24946c90526bc975cf102cf65f3bbedaa234
cgarcia-apt <carlos.garcia@atcpj.com>
2020-05-07 12:57:04 -0500

    modificaciones de iphone x +

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dymamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dymamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -50,6 +50,10 @@ self.addEventListener('activate', e=>{
         keys.forEach( key => {
 
             if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+
+            if(key !== DYNAMIC_CACHE && key.includes('dymamic')){
                 return caches.delete(key);
             }
 
